3e3751018c921508d4a09a0dad0f59452dcef5db
mfs-eduardocarmona <liledwa@gmail.com>
2024-07-05 20:48:12 -0400

    fixed bugs regarding static files, modified conf files

--- blec_proj/settings.py
+++ blec_proj/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'frontend/static/')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
 ]
 
 # Default primary key field type
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '640px', // default
+      md: '768px', // default
+      lg: '1024px', // default
+      xl: '1280px', // default
+      '2xl': '1536px', // default
+      '3xl': '1920px', // custom breakpoint
+    },
     extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], // Replace 'YourFontName' with the actual name of your font
+      },
       colors: {
         primary: '#B571FF',
         secondary: '#FF96D8',
